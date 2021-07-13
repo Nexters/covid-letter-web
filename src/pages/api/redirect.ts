c58e54b9b278b282f96042cf008a7dfabe79ab26
yujeongJeon <workingnewjeong@gmail.com>
2021-07-13 22:09:33 +0900

    [Nexters/covid-letter-web#5] 🐛 req 추가

--- src/pages/api/redirect.ts
+++ src/pages/api/redirect.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import {RESPONSE} from '$constants'
 import {Response} from '$types/response'
-import type {NextApiResponse} from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
-export default function handler(res: NextApiResponse<Response<null>>) {
+export default async function handler(
+    _req: NextApiRequest,
+    res: NextApiResponse<Response<null>>,
+) {
     res.status(200).json({
         code: RESPONSE.REDIRECT,
         message: '',
