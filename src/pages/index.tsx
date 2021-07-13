407695c101d2e33b4f0150ed900db20cd27e1d46
yujeongJeon <workingnewjeong@gmail.com>
2021-07-13 22:01:59 +0900

    [Nexters/covid-letter-web#5] 🐛 userContext, gitignore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ErrorBoundary, {FallbackProps} from '$components/ErrorBoundary'
 import Example from '$components/Example'
-import {useUserContext} from 'contexts/userContext'
+import {useUserContext} from 'contexts/UserContext'
 
 const Fallback = ({error}: FallbackProps) => {
     return (
