74a07cbcf3ed048a326d7b2849c7ce7a2131750a
yujeongJeon <workingnewjeong@gmail.com>
2021-07-13 16:10:24 +0900

    [Nexters/covid-letter-web#13] 🔧 .env에 API key 설정

--- src/config/local.js
+++ src/config/local.js
@@ -1,8 +1,10 @@
+const {REACT_APP_NAVER_CLIENT_ID, REACT_APP_NAVER_CLIENT_SECRET} = process.env
+
 module.exports = {
     OAUTH: {
         NAVER: {
-            CLIENT_ID: '',
-            CLIENT_SECRET: '',
+            CLIENT_ID: REACT_APP_NAVER_CLIENT_ID,
+            CLIENT_SECRET: REACT_APP_NAVER_CLIENT_SECRET,
         },
     },
 }
