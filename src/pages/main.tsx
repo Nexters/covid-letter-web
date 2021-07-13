72c88a14ac167fa3d338fb61570b161e161b9e95
yujeongJeon <workingnewjeong@gmail.com>
2021-07-13 23:50:44 +0900

    [Nexters/covid-letter-web#13] 💄 main.tsx UI 예시

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,17 +1,37 @@
 import useRequest from '$hooks/useRequest'
 import {ProfileResponse} from '$types/login/naver'
 
-interface MainProps {
-    token: string
-}
-
-const Main = ({token}: MainProps) => {
+const Main = () => {
     const {data, error} = useRequest<ProfileResponse>({
         url: '/login/naver/profile',
     })
 
     if (error) throw error
     if (!data) return <div>loading...</div>
-    return <div>Welcome to Covid Letter! Current Access Token is {token}</div>
+
+    const setGender = (g: string | undefined) => {
+        switch (g) {
+            case 'F':
+                return '여성'
+            case 'M':
+                return '남성'
+            default:
+                return '미정'
+        }
+    }
+
+    const {
+        result: {
+            response: {name, gender, age, email},
+        },
+    } = data
+    return (
+        <div>
+            <p>반갑습니다 {name}님!</p>
+            <p>성별: {setGender(gender)}</p>
+            <p>연령대 : {age}</p>
+            <p>이메일: {email}</p>
+        </div>
+    )
 }
 export default Main
