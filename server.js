b7bcc533baf9e1641bbe7643d7573df4d61b7b74
yujeongJeon <workingnewjeong@gmail.com>
2021-07-14 18:01:56 +0900

    🔧 server.js 추가

---
+++ server.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express')
+const next = require('next')
+const port = 3000
+const app = next({dev})
+const handle = app.getRequestHandler()
+
+app.prepare()
+    .then(() => {
+        const server = express()
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        server.listen(port, (error) => {
+            if (error) throw error
+            console.log(`Server stated on http://localhost:${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err.stack)
+        process.exit(1)
+    })
