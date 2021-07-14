93a6c7d05773181973546068f0a55750fe1ff3a4
yujeongJeon <workingnewjeong@gmail.com>
2021-07-14 18:04:19 +0900

    🐛 server.js dev 선언

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const next = require('next')
 const port = 3000
+const dev = process.env.REACT_APP_ENV !== 'production'
 const app = next({dev})
 const handle = app.getRequestHandler()
 
