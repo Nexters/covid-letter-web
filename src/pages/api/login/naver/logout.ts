af0a352c0f9e2770f4d44e40cf32685f2cf8dd9f
yujeongJeon <workingnewjeong@gmail.com>
2021-07-15 23:54:24 +0900

    [Nexters/covid-letter-web#13] 💡 로그아웃 todo 주석 추가

--- src/pages/api/login/naver/logout.ts
+++ src/pages/api/login/naver/logout.ts
@@ -6,7 +6,9 @@ const routes = async (
     _req: NextApiRequest,
     res: NextApiResponse<Response<string>>,
 ) => {
-    /** 쿠키와 BE 세션만 삭제 */
+    /**
+     * @todo 쿠키와 BE 세션만 삭제
+     *  */
     /** 네이버는 연동해제해야 다른 아이디로 로그인 가능, 하지만 연동해제하면 동의가 철회됨 */
     res.setHeader('Set-Cookie', `access_token=; path=/; expires=-1`)
     res.status(200).json(createResponse('ok'))
