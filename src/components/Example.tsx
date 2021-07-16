d60f6eac591a46bd82ed3e76c368734e78070548
siyeon <sylee@megazone.com>
2021-07-16 21:07:58 +0900

    [Nexters/covid-letter-web#9] :lipstick: css 적용 예제 추가

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -2,6 +2,17 @@ import {User} from '$types/response/user'
 import {withAxios} from '$utils/fetcher/withAxios'
 import React, {useState} from 'react'
 import useAsyncError from 'hooks/useAsyncError'
+import { Button } from 'antd';
+import {css} from '@emotion/react'
+import tw from 'twin.macro'
+
+const buttonStyle = css`
+  background-color: coral;
+`
+
+const twButtonStyle = tw`
+    bg-blue-100 w-full
+`
 
 const Counter = () => {
     const [count, setCount] = useState<number>(0)
@@ -10,7 +21,9 @@ const Counter = () => {
         <>
             <div>{count}</div>
             <div>
-                <button onClick={increase}>+</button>
+                <Button onClick={increase} className="bg-blue-700">+</Button>
+                <Button onClick={increase} type="primary" css={buttonStyle}>+</Button>
+                <Button onClick={increase} type="primary" css={twButtonStyle}>+</Button>
             </div>
         </>
     )
