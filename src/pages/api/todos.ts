500f99cc6b4e28e2e9a3d93c540e961c9fc94498
inhyuck <dlsgur1447@gmail.com>
2021-07-21 00:31:03 +0900

    [Nexters/covid-letter-web#31] ✨ json-server 사용하여 todos mock API 호출

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -2,35 +2,18 @@ import {NextApiRequest, NextApiResponse} from 'next'
 import {RESPONSE} from '$constants'
 import {Response} from '$types/response'
 import {Todo} from '$types/todos'
+import Axios from 'axios'
 
-export default function handler(
+export default async function handler(
     _req: NextApiRequest,
     res: NextApiResponse<Response<Partial<Todo[]>>>,
 ) {
+    //todo: be api 호출은 어떤식으로 처리하면 좋을까요?
+    const {data: todos}: {data: Todo[]} = await Axios.get('http://localhost:3001/todos')
+
     res.status(200).json({
         code: RESPONSE.NORMAL,
         message: 'your todos!!',
-        result: [
-            {
-                id: '69e1addf-32d4-41ed-9cbc-f743ab4fc905',
-                contents: 'nextjs 공부하기',
-                is_completed: true,
-            },
-            {
-                id: '1e38d9e6-b17b-44c4-b351-2ad341427463',
-                contents: 'typescript 공부하기',
-                is_completed: false,
-            },
-            {
-                id: 'af59ec08-8276-40fb-9550-538017f8b5d0',
-                contents: 'react 공부하기',
-                is_completed: true,
-            },
-            {
-                id: 'f5944d57-59be-402a-b323-ce11c8f613bb',
-                contents: 'emotion 공부하기',
-                is_completed: false,
-            },
-        ],
+        result: todos,
     })
 }
