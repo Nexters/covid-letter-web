af4186be32465e094980a8bef4f52f9b6c2ba133
siyeon <sylee@megazone.com>
2021-07-22 04:29:15 +0900

    [Nexters/covid-letter-web#33] :sparkles: Todo list를 위한 context, hook 추가

---
+++ src/contexts/TodoListContext.tsx
@@ -0,0 +1,57 @@
+import {createContext, ReactNode, useEffect, useState} from 'react'
+import useRequest from '$hooks/useRequest'
+
+export interface Todo {
+    id: number
+    item: string
+    complete: boolean
+}
+
+export interface TodoListContextState {
+    todoList: Todo[] | undefined
+    remove: (id: number) => void
+    add: (item: string) => void
+}
+
+export const TodoListContext = createContext<TodoListContextState>(
+    {} as TodoListContextState,
+)
+
+export const TodoListProvider = ({children}: {children: ReactNode}) => {
+    const {data: todoList} = useRequest<Todo[]>(
+        {
+            url: '/todo',
+        },
+        {
+            revalidateOnMount: true,
+        },
+    )
+    useEffect(() => {
+        if (!todoList) return
+    }, [todoList])
+
+    function remove(id: number) {
+        const idx = todoList?.findIndex((d) => d.id === id)
+        const clonedList: Todo[] = JSON.parse(JSON.stringify(todoList))
+        if (idx != null) {
+            clonedList.splice(idx, 1)
+        }
+    }
+    function add(item: string) {
+        const newItem: Todo = {
+            item,
+            id: todoList?.length || 0,
+            complete: false,
+        }
+    }
+    return (
+        <TodoListContext.Provider
+            value={{
+                todoList,
+                remove,
+                add,
+            }}>
+            {children}
+        </TodoListContext.Provider>
+    )
+}
