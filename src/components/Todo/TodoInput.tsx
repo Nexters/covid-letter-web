c8900d602e6fd7c924637fa9e046b0848e7ee001
siyeon <sylee@megazone.com>
2021-07-22 16:24:25 +0900

    [Nexters/covid-letter-web#33] :sparkles: forwardRef를 이용한 input 컴포넌트 추가

--- src/components/Todo/TodoInput.tsx
+++ src/components/Todo/TodoInput.tsx
@@ -1,5 +1,25 @@
-const TodoInput = () => {
-    return <>Input</>
-}
+import {forwardRef, useImperativeHandle, useState} from 'react'
+import {useTodoListContext} from '$hooks/useTodoList'
+
+const TodoInput = forwardRef((_, ref) => {
+    const {add} = useTodoListContext()
+
+    const [value, setValue] = useState('')
+    useImperativeHandle(ref, () => ({value}), [value])
+
+    const handleKeyPress = ({key}: {key: string}) => {
+        if (key === 'Enter') {
+            add(value)
+            setValue('')
+        }
+    }
+    return (
+        <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyPress={handleKeyPress}
+        />
+    )
+})
 
 export default TodoInput
