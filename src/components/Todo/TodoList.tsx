653262eb21596858a876c62d09aab14eccff1265
siyeon <sylee@megazone.com>
2021-07-22 16:29:30 +0900

    [Nexters/covid-letter-web#33] :sparkles: forwardRef를 이용한 input 컴포넌트 추가

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -2,19 +2,16 @@ import TodoItem from '$components/Todo/TodoItem'
 import {Todo} from '../../contexts/TodoListContext'
 import {useTodoListContext} from '$hooks/useTodoList'
 import TodoInput from '$components/Todo/TodoInput'
+import {useRef} from 'react'
 
 const TodoList = () => {
     const {todoList} = useTodoListContext()
+    const inputRef = useRef('')
     return (
         <div>
-            <TodoInput />
+            <TodoInput ref={inputRef} />
             {todoList?.map((d: Todo) => (
-                <TodoItem
-                    key={d.id}
-                    id={d.id}
-                    item={d.item}
-                    complete={d.complete}
-                />
+                <TodoItem key={d.id} id={d.id} item={d.item} />
             ))}
         </div>
     )
