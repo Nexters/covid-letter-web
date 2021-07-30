f7bbb4e8215a675cec63f75700d8a8a44a956da1
yujeongJeon <workingnewjeong@gmail.com>
2021-07-31 02:37:01 +0900

    [Nexters/covid-letter-web#38] 🔧 ✅ 스냅샷 테스트 작성

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,23 @@
 import '@testing-library/jest-dom'
 import '@testing-library/jest-dom/extend-expect'
 
-jest.mock('next/config', () => () => ({
-    publicRuntimeConfig: {
-        URL: {},
-    },
-}))
+jest.mock('next/config', () => () => {
+    const config = jest.requireActual('next/config')
+
+    return {
+        ...config,
+        publicRuntimeConfig: {
+            REACT_APP_ENV: 'local',
+            OAUTH: {
+                NAVER: {
+                    CLIENT_ID: 'naver-client-id',
+                    CLIENT_SECRET: 'naver-client-secret',
+                },
+                GOOGLE: {
+                    CLIENT_ID: 'google-client-id',
+                    API_KEY: 'google-api-key',
+                },
+            },
+        },
+    }
+})
