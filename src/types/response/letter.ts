86bf3120a63d4b08cd4b0c9d8d4d563a6d36db62
siyeon <sylee@megazone.com>
2021-08-03 02:28:41 +0900

    [Nexters/covid-letter-web#37] :label: Letter 타입 추가

--- src/types/response/letter.ts
+++ src/types/response/letter.ts
@@ -1,19 +1,59 @@
-/**
- * 편지 상태
- */
-export enum LetterState {
-    PENDING = 'PENDING', //전송 대기
-    SEND = 'SEND', //전송 완료(읽기 전)
-    DISPLAYED = 'DISPLAYED', //전송완료(읽음)
-}
+const LETTER_STATE = {
+    SEND: 'SEND',
+    PENDING: 'PENDING',
+} as const
+type LETTER_STATE = typeof LETTER_STATE[keyof typeof LETTER_STATE]
+
+const STICKER = {
+    HAPPY: 'HAPPY',
+    EXPECT: 'EXPECT',
+    SHY: 'SHY',
+    LOVE: 'LOVE',
+    UNHAPPY: 'UNHAPPY',
+    SAD: 'SAD',
+    SHOCK: 'SHOCK',
+    BLUE: 'BLUE',
+    SHAME: 'SHAME',
+    FIGHTING: 'FIGHTING',
+    GOOD: 'GOOD',
+    OK: 'OK',
+} as const
+type STICKER = typeof STICKER[keyof typeof STICKER]
 
 export interface Letter {
-    title: string,
-    contents: string,
-    email: string,
-    state: LetterState,
-    sticker: string,
-    questionId: number,
-    encryptedId: string,
-    createdDate: string,
+    answer: string
+    contents: string
+    createdDate: string
+    email: string
+    encryptedId: string
+    id: number
+    letterTo: string
+    questionId: string
+    state: LETTER_STATE
+    sticker: STICKER
+    title: string
+    updateDate: string
+}
+
+export interface LetterResponse {
+    email: string
+    letters: Letter[]
+    name: string
+}
+
+export interface Question {
+    id: number
+    text: string
+}
+
+export interface LetterOption {
+    covidStat: number
+    questions: Question[]
+    text: string
+}
+
+export interface LetterOptionResponse {
+    result: LetterOption[]
+    code: string
+    message: string
 }
