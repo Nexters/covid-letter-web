d6dd475824b3914140ce52ef662f2f95be1fee8b
yujeongJeon <workingnewjeong@gmail.com>
2021-08-03 23:27:59 +0900

    [Nexters/covid-letter-web#34] 💄 네이버 로그인 버튼

--- src/components/login/NaverLoginButton.tsx
+++ src/components/login/NaverLoginButton.tsx
@@ -3,7 +3,25 @@ import {HOST_URL} from '$config'
 import {AuthorizeResponse} from '$types/login/naver'
 import ROUTES from '$constants/routes'
 import useAsyncError from '$hooks/useAsyncError'
-import {Button} from 'antd'
+import tw from 'twin.macro'
+import styled from '@emotion/styled'
+import SvgNaver from 'assets/NaverLogo'
+
+const commonTw = tw`
+    tw-flex tw-text-center tw-flex-1 tw-justify-center tw-items-center
+`
+
+const Button = styled.button`
+    width: 100%;
+    padding: 1.85rem 0;
+    color: #767678;
+    border: 1px solid #e6e6ea;
+    border-radius: 0.4rem;
+`
+
+const buttonTw = tw`
+tw-font-ohsquare tw-font-bold tw-text-base tw-bg-grey-000
+`
 
 interface NaverLoginButtonProps {
     returnUrl: string
@@ -32,8 +50,9 @@ const NaverLoginButton = ({returnUrl}: NaverLoginButtonProps) => {
         }
     }
     return (
-        <Button type={'primary'} block onClick={() => handleLogin()}>
-            네이버로 로그인
+        <Button css={{...buttonTw, ...commonTw}} onClick={() => handleLogin()}>
+            <SvgNaver style={{marginRight: '24px'}} />
+            네이버로 계속하기
         </Button>
     )
 }
