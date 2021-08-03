2b89fc6594dfebf0a8549b524f84842d464f5588
yujeongJeon <workingnewjeong@gmail.com>
2021-08-03 21:45:11 +0900

    [Nexters/covid-letter-web#34] 💄 로그인 Title, SubTitle

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,20 +7,31 @@ import tw from 'twin.macro'
 const Container = styled.div``
 
 const Titile = styled.div`
-    padding-top: 80px;
+    padding-top: 8rem;
 `
-const titleTwStyles = tw`
-    tw-flex tw-text-center tw-flex-1 tw-justify-center tw-items-center
+const titleTw = tw`
+    tw-flex tw-text-center tw-flex-1 tw-justify-center tw-items-center tw-text-xl
+`
+
+const SubTitle = styled.div`
+    margin-top: 1.2rem;
+`
+
+const subtitleTw = tw`
+tw-flex tw-text-center tw-flex-1 tw-justify-center tw-items-center tw-text-sm
 `
 
 const Login = () => {
     return (
         <Container>
-            <Titile css={titleTwStyles}>
+            <Titile css={titleTw}>
                 나에게 보내는 편지
                 <br />
                 ‘코로나’편
             </Titile>
+            <SubTitle css={subtitleTw}>
+                어려운 시기, 미래의 나에게 마음을 담아 전해요.
+            </SubTitle>
             <NaverLoginButton returnUrl={ROUTES.COVID.MAIN} />
             <GoogleLoginButton returnUrl={ROUTES.COVID.MAIN} />
         </Container>
