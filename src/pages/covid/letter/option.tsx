b8d8b06ba751ecefafb07b3747c670f86c0a8bc0
siyeon <sylee@megazone.com>
2021-08-06 03:25:07 +0900

    [Nexters/covid-letter-web#37] :sparkles: Option 선택 화면 마크업

--- src/pages/covid/letter/option.tsx
+++ src/pages/covid/letter/option.tsx
@@ -1,21 +1,101 @@
 import {LetterOption} from '$types/response/letter'
 import {withAxios} from '$utils/fetcher/withAxios'
+import styled from '@emotion/styled'
+import {css, jsx} from '@emotion/react'
+import tw from 'twin.macro'
+import {useState} from 'react'
+
+const Container = styled.section`
+    // tw\`tw-bg-beige-300\`,
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 360px;
+    height: 100vh;
+    margin: 0 auto;
+    background: #eae6d7;
+`
+
+const Title = styled.h3`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 24px;
+    padding-left: 24px;
+    padding-right: 24px;
+    .sub-title {
+        margin-top: 8px;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+    }
+`
+
+const ButtonList = styled.ul`
+    display: inherit;
+    align-self: center;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 48px;
+`
+
+const Button = styled.button([
+    // tw`tw-bg-yellow-600`,
+    css`
+        min-width: 312px;
+        height: 52px;
+        border-radius: 4px;
+    `,
+    ({isClicked}) =>
+        isClicked
+            ? css`
+                  background: #d1a65a;
+                  border: 2px solid #11373e; //tw-border
+                  font-weight: bold;
+              `
+            : css`
+                  background: #eae6d7;
+                  border: 1px solid #d4cec4;
+              `,
+])
+
+const ConfirmButton = styled.button`
+    position: fixed;
+    min-width: 360px;
+    height: 52px;
+    background: #11373e;
+    bottom: 0;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 25px;
+`
 
 interface Props {
-    options: string[]
+    options: LetterOption[]
 }
-const onClickOption = () => {
-    console.log('click option')
-}
-const LetterOptionPage = ({options}: Props) => {
+
+const LetterOptionPage = ({options}: Props, {isMobile}: {isMobile: boolean}) => {
+    const [selectedOptionId, setSelectedOptionId] = useState<number>(-1)
+    const handleClick = (option: LetterOption) => {
+        if (option.id === selectedOptionId) {
+            setSelectedOptionId(-1)
+        } else setSelectedOptionId(option.id)
+    }
     return (
-        <>
-            {options.map((option) => (
-                <li key={option} onClick={onClickOption}>
-                    {option}
-                </li>
-            ))}
-        </>
+        <Container>
+            <Title>
+                발송 기준 선택 📮
+                <h4 className="sub-title">언제 발송을 원하시나요?</h4>
+            </Title>
+            <ButtonList>
+                {options.map((option, index) => (
+                    <li key={option.id} onClick={() => handleClick(option)}>
+                        <Button isClicked={index === selectedOptionId}>{option.text}</Button>
+                    </li>
+                ))}
+            </ButtonList>
+            <ConfirmButton>확인</ConfirmButton>
+        </Container>
     )
 }
 
@@ -25,7 +105,9 @@ export async function getStaticProps() {
         method: 'GET',
     })
 
-    const options = res.map((d) => d.text)
+    const options = res.map((option) => ({
+        ...option,
+    }))
 
     return {props: {options}}
 }
