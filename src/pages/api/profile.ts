71e43f42fe36ae1f87df5c5d2c018f933dce8cad
yujeongJeon <workingnewjeong@gmail.com>
2021-08-09 20:50:00 +0900

    [Nexters/covid-letter-web#45] 🤡 /profile api

--- src/pages/api/profile.ts
+++ src/pages/api/profile.ts
@@ -1,18 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {API_URL_BASE} from '$config/index'
 import {RESPONSE} from '$constants'
 import {Response} from '$types/response'
 import {User} from '$types/response/user'
-import axios, {AxiosResponse} from 'axios'
-import {ServerResponse} from '$types/response'
 import type {NextApiRequest, NextApiResponse} from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Response<Partial<User>>>) {
-    const {accessToken} = req.query
-    const headers = {
-        Authorization: `${accessToken}`,
-        'User-Agent': req.headers['user-agent'],
-    }
+export default async function handler(_req: NextApiRequest, res: NextApiResponse<Response<Partial<User>>>) {
+    // const {accessToken} = req.query
+    // const headers = {
+    //     Authorization: `Bearer ${accessToken}`,
+    //     'User-Agent': req.headers['user-agent'],
+    // }
 
     // const {
     //     data: {data},
