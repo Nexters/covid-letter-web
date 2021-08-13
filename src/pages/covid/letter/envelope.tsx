74470767c450e26e018e426505796cffd5c88e81
yujeongJeon <workingnewjeong@gmail.com>
2021-08-13 16:01:58 +0900

    🚨 hasCloseButton 제거

--- src/pages/covid/letter/envelope.tsx
+++ src/pages/covid/letter/envelope.tsx
@@ -12,36 +12,38 @@ import ROUTES from '$constants/routes'
  * background 비어있는 상태로 편지 봉투 모달만 보임
  */
 const Envelope = ({letter}: {letter: Letter}) => {
-    const {title, state, sticker, createdDate, encryptedId} = letter;
-    const isAvailableOpenLetter = state !== LetterState.PENDING;
+    const {title, state, sticker, createdDate, encryptedId} = letter
+    const isAvailableOpenLetter = state !== LetterState.PENDING
 
     const router = useRouter()
     const {alert} = useAlertStore()
     const openLetter = () => {
         if (!isAvailableOpenLetter) {
             alert({
-                title: '[TEXT 수정필요] 아직 편지를 열어 볼 수 없어요'
+                title: '[TEXT 수정필요] 아직 편지를 열어 볼 수 없어요',
             })
-            return;
+            return
         }
 
         router.push({pathname: ROUTES.COVID.LETTER.DETAIL, query: {encryptedId}})
     }
 
     return (
-        <HalfLayer isShow={true} closeFn={() => {}} hasCloseButton={false} >
+        <HalfLayer isShow={true} closeFn={() => {}}>
             <div>제목: {title}</div>
             <div>작성일: {convertCommonDateFormat(createdDate)}</div>
             <div>발송기준: -</div>
             <div>전송상태: {state} - UI 적용</div>
             <div>스티커: {sticker} - UI 적용</div>
-            <button /**disabled={!isAvailableOpenLetter}**/ onClick={openLetter}>{isAvailableOpenLetter ? '뜯어볼래요' : '아직 열어볼 수 없어요'}</button>
+            <button /**disabled={!isAvailableOpenLetter}**/ onClick={openLetter}>
+                {isAvailableOpenLetter ? '뜯어볼래요' : '아직 열어볼 수 없어요'}
+            </button>
         </HalfLayer>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const {id: encryptedId} = context.query;
+    const {id: encryptedId} = context.query
     const letter = await withAxios<Letter>({
         url: `/letters/${encryptedId}`,
         method: 'GET',
@@ -50,8 +52,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
         props: {
             letter,
-        }
+        },
     }
 }
 
-export default Envelope;
+export default Envelope
