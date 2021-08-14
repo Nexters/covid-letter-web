090fd2a04ea689a9b8a6b95439309bd30f58181a
siyeon <sylee@megazone.com>
2021-08-15 02:13:40 +0900

    [Nexters/covid-letter-web#55] :label: letterStore 수정

--- src/stores/Letter.ts
+++ src/stores/Letter.ts
@@ -2,6 +2,7 @@ import {STICKER} from '$types/response/letter'
 
 interface NewLetter {
     answer: string
+    title: string
     optionId?: number
     questionId?: number
     sticker?: STICKER
@@ -9,12 +10,14 @@ interface NewLetter {
 
 export interface LetterState extends NewLetter {
     addAnswer: (answer: string) => void //변수명 고민..
+    addTitle: (title: string) => void
     resetAnswer: () => void
 }
 
 const createLetter = (): LetterState => {
     return {
         answer: '',
+        title: '',
         optionId: 0,
         questionId: 0,
         sticker: 'OK',
@@ -23,9 +26,14 @@ const createLetter = (): LetterState => {
             this.answer = answer || ''
         },
 
+        addTitle(title: string) {
+            this.title = title || ''
+        },
+
         resetAnswer: () => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
+            // @ts-ignore 이거 처리가 너무 어려워요 8ㅅ8 도와주세욥 ㅠㅠ
+            this.title = ''
             this.answer = ''
         },
     }
