4398d3d32eb9e1e2fac66b6e9e78f66647f958b2
inhyuck <dlsgur1447@gmail.com>
2021-08-15 20:42:44 +0900

    [Nexters/covid-letter-web#47] ✨ 스티커 Svg 적용

---
+++ src/pages/test/stickers.tsx
@@ -0,0 +1,33 @@
+import SvgStickerBlue from '../../assets/sticker/Blue'
+import SvgStickerExpect from '../../assets/sticker/Expect'
+import SvgStickerFighting from '../../assets/sticker/Fighting'
+import SvgStickerGood from '../../assets/sticker/Good'
+import SvgStickerHappy from '../../assets/sticker/Happy'
+import SvgStickerLove from '../../assets/sticker/Love'
+import SvgStickerOk from '../../assets/sticker/Ok'
+import SvgStickerSad from '../../assets/sticker/Sad'
+import SvgStickerShame from '../../assets/sticker/Shame'
+import SvgStickerShock from '../../assets/sticker/Shock'
+import SvgStickerShy from '../../assets/sticker/Shy'
+import SvgStickerUnhappy from '../../assets/sticker/Unhappy'
+
+const Stickers = () => {
+    return (
+        <>
+            <SvgStickerBlue width={'5rem'} height={'5rem'} />
+            <SvgStickerExpect width={'5rem'} height={'5rem'} />
+            <SvgStickerFighting width={'5rem'} height={'5rem'} />
+            <SvgStickerGood width={'5rem'} height={'5rem'} />
+            <SvgStickerHappy width={'5rem'} height={'5rem'} />
+            <SvgStickerLove width={'5rem'} height={'5rem'} />
+            <SvgStickerOk width={'5rem'} height={'5rem'} />
+            <SvgStickerSad width={'5rem'} height={'5rem'} />
+            <SvgStickerShame width={'5rem'} height={'5rem'} />
+            <SvgStickerShock width={'5rem'} height={'5rem'} />
+            <SvgStickerShy width={'5rem'} height={'5rem'} />
+            <SvgStickerUnhappy width={'5rem'} height={'5rem'} />
+        </>
+    )
+}
+
+export default Stickers
