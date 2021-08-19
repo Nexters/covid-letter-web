0f3604611e565484bebb998ca4b8c5f14e6d2192
siyeon <sylee@megazone.com>
2021-08-19 23:16:18 +0900

    [Nexters/covid-letter-web#64] :sparkles: 완료 화면에 store 적용 및 css 수정

--- src/pages/covid/letter/new/finish.tsx
+++ src/pages/covid/letter/new/finish.tsx
@@ -3,28 +3,38 @@ import tw from 'twin.macro'
 import {observer} from 'mobx-react-lite'
 import {useLetterStore} from '$contexts/StoreContext'
 import {getCurrentDate} from '$utils/date'
+import {StickerFactory} from '$components/sticker/stickerFactory'
+import {useProfileContext} from '$contexts/ProfileContext'
 
 const Finish = () => {
-    const {title} = useLetterStore()
+    const {title, optionText, sticker} = useLetterStore()
+    const {profile} = useProfileContext()
     return (
         <Container>
             <h3>
                 편지 작성을 완료했어! <br /> 소중히 보관했다가 <br /> 안전하게 보내줄게 :)
             </h3>
             <ResultWrapper>
-                <span className="letter-title">{title}</span>
-                <LetterDescription>
-                    <div className="desc-wrapper">
-                        <span className="name">To. 사용자 이름</span>
-                        <span className="email">***@***.com</span>
-                        <span className="option">발송 기준</span>
-                        <span className="option-text">발송 기준 1</span>
-                    </div>
-                    <div className="img-square">이미지 테스트</div>
-                </LetterDescription>
+                <DescriptionWrapper>
+                    <p className="title-wrapper">
+                        <span className="title">{title}</span>
+                        <span className="title-sticker">{StickerFactory(sticker.type, '2.4rem')}</span>
+                    </p>
+                    <LetterDescription>
+                        <div className="desc-wrapper">
+                            <span className="name">To. {profile?.name}</span>
+                            <span className="email">{profile?.email}</span>
+                            <span className="option">발송 기준</span>
+                            <span className="option-text">{optionText}</span>
+                        </div>
+                        <div className="img-square">이미지 테스트</div>
+                    </LetterDescription>
+                </DescriptionWrapper>
                 <Divider />
-                <p className="create-date">{getCurrentDate()}</p>
-                <p className="letter-responder">시연</p>
+                <ResponderWrapper>
+                    <p className="create-date">{getCurrentDate()}</p>
+                    <p className="letter-responder">{profile?.name}</p>
+                </ResponderWrapper>
             </ResultWrapper>
             <ConfirmButton>편지 기다리기</ConfirmButton>
         </Container>
@@ -46,7 +56,7 @@ const Container = styled.div`
 `
 
 const ResultWrapper = styled.section`
-    ${tw`tw-bg-grey-000`}
+    ${tw`tw-bg-grey-000 tw-flex tw-flex-col tw-justify-between`}
     width: 100%;
     max-width: 42rem;
     max-height: calc(100vh - 23.6rem);
@@ -55,25 +65,25 @@ const ResultWrapper = styled.section`
     bottom: 5.2rem;
     border-radius: 1rem 1rem 0 0;
     padding: 4rem 2.4rem 3.8rem;
-    .letter-title {
-        ${tw`tw-text-grey-800`}
+`
+
+const DescriptionWrapper = styled.article`
+    .title-wrapper {
+        display: inline-flex;
+        width: 100%;
+    }
+    .title {
+        ${tw`tw-text-grey-800 tw-truncate`}
         font-size: 1.6rem;
         line-height: 2.5rem;
+        margin-right: 1.2rem;
+        max-width: calc(100% - 2.4rem);
     }
-    .create-date {
-        ${tw`tw-font-ohsquare-air tw-text-grey-600`}
-        text-align: right;
-        font-size: 1.4rem;
-        line-height: 2.2rem;
-    }
-    .letter-responder {
-        ${tw`tw-font-ohsquare-air tw-text-grey-800`}
-        text-align: right;
-        font-size: 1.4rem;
-        line-height: 2.2rem;
+    .title-sticker {
+        flex-shrink: 0;
     }
 `
-const LetterDescription = styled.article`
+const LetterDescription = styled.div`
     ${tw`tw-flex tw-justify-between`}
     letter-spacing: -0.015em;
     margin-top: 3.4rem;
@@ -107,8 +117,23 @@ const LetterDescription = styled.article`
 
 const Divider = styled.hr`
     ${tw`tw-border-grey-200`}
-    border-width: 0.3rem;
+    border-top-width: 0.3rem;
     margin-bottom: 3.2rem;
+`
+
+const ResponderWrapper = styled.article`
+    .create-date {
+        ${tw`tw-font-ohsquare-air tw-text-grey-600`}
+        text-align: right;
+        font-size: 1.4rem;
+        line-height: 2.2rem;
+    }
+    .letter-responder {
+        ${tw`tw-font-ohsquare-air tw-text-grey-800`}
+        text-align: right;
+        font-size: 1.4rem;
+        line-height: 2.2rem;
+    }
 `
 
 const ConfirmButton = styled.button`
