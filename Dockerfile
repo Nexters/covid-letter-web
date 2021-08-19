dd9f0c76f72af1f3391b47700ee096ac02c326e7
yujeongJeon <workingnewjeong@gmail.com>
2021-08-19 23:27:16 +0900

    🔧 nextjs 프로젝트 docker build & run

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:alpine
+
+EXPOSE 3000
+
+ARG REACT_APP_ENV
+
+WORKDIR /app/admin
+
+COPY ./package*.json ./
+RUN  npm install pm2 -g
+
+RUN  npm ci
+
+COPY ./ ./
+
+RUN echo "Based on env: $REACT_APP_ENV"
+
+RUN npm run build:${REACT_APP_ENV}
+
+ENTRYPOINT pm2 list && REACT_APP_ENV=${REACT_APP_ENV} pm2-runtime start pm2.config.js
---
+++ pm2.config.js
@@ -0,0 +1,37 @@
+const PROFILES = {
+    local: {
+        instances: 1,
+        node_args: '--trace-warnings',
+    },
+    production: {
+        instances: 4,
+    },
+}
+
+function createPm2Config() {
+    const profile = process.env.REACT_APP_ENV
+    const envConfig = PROFILES[profile]
+    return {
+        apps: [
+            {
+                name: 'node-web-server',
+                script: 'node_modules/next/dist/bin/next',
+                args: 'start',
+                cwd: './',
+                interpreter: 'node',
+                exec_mode: 'cluster',
+                max_restarts: 5,
+                min_uptime: 5000,
+                log_date_format: '<YYYY-MM-DD HH:mm:ss>',
+                env: {
+                    NODE_ENV: 'production',
+                    REACT_APP_ENV: profile || 'local',
+                },
+                ...envConfig,
+            },
+        ],
+    }
+}
+console.log(JSON.stringify(createPm2Config(), null, '\t'))
+
+module.exports = createPm2Config()
