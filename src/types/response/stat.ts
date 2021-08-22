b2105f1a2b7002a54d5c413abb146398afe32ca7
yujeongJeon <workingnewjeong@gmail.com>
2021-08-22 16:29:52 +0900

    [Nexters/covid-letter-web#48] 🔥 /covid/stats(임시) api

--- src/types/response/stat.ts
+++ src/types/response/stat.ts
@@ -1,17 +1,3 @@
-export interface CovidStats {
-    completeShot: number
-    confirmedCase: number
-    completeCure: number
-    shotRate: number
-    confirmedIncrease: number
-    cureIncrease: number
-}
-
-export interface LetterStats {
-    unsented: number
-    sented: number
-}
-
 export type CovidStatResponse<T = number | string> = {
     date: string
 
