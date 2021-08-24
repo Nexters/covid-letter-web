35f38cab01010f568db30eb4161869de32e257e9
siyeon <sylee@megazone.com>
2021-08-24 23:49:03 +0900

    [Nexters/covid-letter-web#101] :sparkles: 유저 이탈 방지 모달 추가, 헤더 수정 및 키보드 뷰 제거

--- src/pages/covid/letter/new/[optionId].tsx
+++ src/pages/covid/letter/new/[optionId].tsx
@@ -7,12 +7,12 @@ import cookies from 'next-cookies'
 import {GetServerSideProps} from 'next'
 import Answer from '$components/question/Answer'
 import ROUTES from '$constants/routes'
-import {useLetterStore} from '$contexts/StoreContext'
+import {useAlertStore, useLetterStore} from '$contexts/StoreContext'
 import {useRouter} from 'next/router'
-import {ReactChild, useEffect, useRef, useState} from 'react'
-import useResizeObserver from '$hooks/useResizeObserver'
-import {css} from '@emotion/react'
+import {useEffect} from 'react'
 import {observer} from 'mobx-react-lite'
+import {HEADER_POSITION, HEADER_TYPE} from '$components/header/constants'
+import CommonHeader from '$components/header/CommonHeader'
 
 interface Props {
     questions: Question[]
@@ -20,15 +20,13 @@ interface Props {
 
 const NewLetter = ({questions}: Props) => {
     const router = useRouter()
-    const {answer, title, optionId} = useLetterStore()
+    const {answer, title, optionId, resetStore} = useLetterStore()
+    const {confirm} = useAlertStore()
     useEffect(() => {
         if (!optionId) {
             router.push({pathname: ROUTES.COVID.LETTER.OPTION})
         }
     }, [])
-    const [viewportHeight, setViewportHeight] = useState(0)
-    const [targetHeight, setTargetHeight] = useState(0)
-    const [isKeyboardView, setIsKeyboardView] = useState(false)
     const onClickConfirm = () => {
         if (answer.length === 0 || title.length === 0) return
         router.push({
@@ -36,20 +34,25 @@ const NewLetter = ({questions}: Props) => {
             query: {optionId: router.query.optionId},
         })
     }
-    const ref = useRef(null)
-    const callback = (entry: any) => {
-        setTargetHeight(entry.height)
-        setViewportHeight(document.body.clientHeight)
-        if (targetHeight / viewportHeight > 1) setIsKeyboardView(true)
-        else setIsKeyboardView(false)
+    const onClickHeaderButton = () => {
+        confirm({
+            title: '아직 편지 내용이 저장되지 않았어!',
+            message: '해당 페이지를 벗어나면\n편지 내용이 저장되지 않고 지워져!\n그래도 뒤로 갈거야?',
+            onSuccess: () => {
+                router.push(ROUTES.COVID.LETTER.OPTION)
+                resetStore()
+            },
+            successText: '응, 할래!',
+            cancelText: '아니, 안할래',
+        })
+        return
     }
-    useResizeObserver(ref, callback)
 
     return (
         <>
-            <Header isKeyboardView={isKeyboardView} />
-            <Container ref={ref}>
-                {isKeyboardView ? <></> : <NewLetterQuestion questions={questions} />}
+            <CommonHeader type={HEADER_TYPE.BACK} position={HEADER_POSITION.LEFT} onClick={onClickHeaderButton} />
+            <Container>
+                <NewLetterQuestion questions={questions} />
                 <div>
                     <Answer />
                     <ConfirmButton onClick={onClickConfirm}>확인</ConfirmButton>
@@ -74,11 +77,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {props: {questions}}
 }
 
-type PropsType = {
-    children?: ReactChild
-    isKeyboardView: boolean
-}
-
 const Container = styled.section`
     ${tw`tw-bg-beige-300 tw-flex tw-flex-col`}
     margin: 0 auto;
@@ -86,16 +84,6 @@ const Container = styled.section`
     min-height: calc(100vh - 5.5rem);
     justify-content: space-between;
 `
-const Header = styled.section<PropsType>(({isKeyboardView}) =>
-    isKeyboardView
-        ? css`
-              transform: translateY(-5.5rem);
-              margin-top: -5.5rem;
-          `
-        : css`
-              height: 5.5rem;
-          `,
-)
 
 const ConfirmButton = styled.button`
     ${tw`tw-relative tw-bg-primary-green-500 tw-bottom-0 tw-text-grey-000 tw-font-bold`}
