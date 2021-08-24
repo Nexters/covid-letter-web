72ed00869d8c35c99ce701a013e283911f37be5e
yujeongJeon <workingnewjeong@gmail.com>
2021-08-25 00:32:47 +0900

    [Nexters/covid-letter-web#107] 🔥 Local 환경에서 무조건 띄우기 제거

--- src/hooks/useTutorial.ts
+++ src/hooks/useTutorial.ts
@@ -1,13 +1,8 @@
-import {NEXT_PUBLIC_ENV} from '$config/index'
 import {useEffect, useState} from 'react'
 
 const useTutorial = (shouldTutorialOpen: boolean) => {
     const [tutorialShow, toggle] = useState(false)
     useEffect(() => {
-        if (NEXT_PUBLIC_ENV === 'local') {
-            toggle(true)
-            return
-        }
         if (shouldTutorialOpen) {
             toggle(true)
         }
