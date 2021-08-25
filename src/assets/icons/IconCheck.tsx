17b37f44f7e5a8214684e997866f5aed0da16f3c
siyeon <sylee@megazone.com>
2021-08-25 21:56:52 +0900

    [Nexters/covid-letter-web#102] :lipstick: 누락 아이콘 추가

--- src/assets/icons/IconCheck.tsx
+++ src/assets/icons/IconCheck.tsx
@@ -8,6 +8,7 @@ function IconCheck() {
             xmlns="http://www.w3.org/2000/svg"
             style={{
                 display: 'inline-block',
+                marginRight: '1.2rem',
             }}>
             <path
                 d="M4 8.5L6.5 11 12 5.5"
