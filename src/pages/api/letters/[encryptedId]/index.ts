ed5634968a629d1abf0d3e88e9dd5d34c6f7950f
siyeon <sylee@megazone.com>
2021-08-25 18:45:31 +0900

    [Nexters/covid-letter-web#119] :sparkles: 부치지못한 편지 api 추가

--- src/pages/api/letters/[encryptedId]/index.ts
+++ src/pages/api/letters/[encryptedId]/index.ts
@@ -5,13 +5,12 @@ import {RESPONSE} from '$constants'
 import axios, {AxiosResponse} from 'axios'
 import {API_URL_BASE} from '$config'
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Response<Letter | null>>
-) {
+const getLetter = async (req: NextApiRequest, res: NextApiResponse<Response<Letter | null>>) => {
     const {encryptedId} = req.query
     try {
-        const {data: {errorCode, message, data: letter}}: AxiosResponse<ServerResponse<Letter>> = await axios.get(`${API_URL_BASE}/letters/${encryptedId}`)
+        const {
+            data: {errorCode, message, data: letter},
+        }: AxiosResponse<ServerResponse<Letter>> = await axios.get(`${API_URL_BASE}/letters/${encryptedId}`)
         if (errorCode) {
             console.info(`errorCode: ${errorCode}, message: ${message}`)
             throw new Error(message)
@@ -28,5 +27,52 @@ export default async function handler(
             message: '편지 상세 조회 실패',
             result: null,
         })
+    }
+}
+
+const saveLetter = async (req: NextApiRequest, res: NextApiResponse<Response<Letter | null>>) => {
+    try {
+        const {encryptedId} = req.query
+        const {sendOptionId} = req.body
+
+        const {
+            data: {errorCode, message, data: letter},
+        }: AxiosResponse<ServerResponse<Letter>> = await axios.put(
+            `${API_URL_BASE}/letters/${encryptedId}`,
+            {sendOptionId},
+            {headers: req.headers},
+        )
+
+        if (errorCode) {
+            console.info(`errorCode: ${errorCode}, message: ${message}`)
+            throw new Error(message)
+        }
+
+        res.status(200).json({
+            code: RESPONSE.NORMAL,
+            message: '편지 발송 성공',
+            result: {
+                ...letter,
+            },
+        })
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            code: RESPONSE.ERROR,
+            message: '편지 발송 실패',
+            result: null,
+        })
+    }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Response<Letter | null>>) {
+    if (req.method === 'GET') {
+        await getLetter(req, res)
+        return
+    }
+
+    if (req.method === 'POST') {
+        await saveLetter(req, res)
+        return
     }
 }
